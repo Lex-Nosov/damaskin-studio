bc393c1d22ccb2d33b71f4a29095ad2e6f47ed8f
Lex <alexnosov2009@rambler.ru>
2023-12-17 02:10:25 +0300

    first day commit

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Services, Clients, Master, Sessions
+
+
+@admin.register(Services)
+class MainAdmin(admin.ModelAdmin):
+    list_display = ['service_name', 'service_description', 'service_cost', 'service_duration']
+
+
+@admin.register(Clients)
+class ClientAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Master)
+class MasterAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Sessions)
+class SessionAdmin(admin.ModelAdmin):
+    pass
--- main_app/models.py
+++ main_app/models.py
@@ -1,1 +1,40 @@
 from django.db import models
+
+
+class Clients(models.Model):
+    client_first_name = models.CharField(max_length=100)
+    client_second_name = models.CharField(max_length=100)
+    client_phone_number = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f'{self.client_first_name}{self.client_second_name}'
+
+
+class Master(models.Model):
+    master_first_name = models.CharField(max_length=50)
+    master_second_name = models.CharField(max_length=50)
+    master_phone_number = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f'{self.master_first_name}{self.master_second_name}'
+
+
+class Services(models.Model):
+    service_name = models.CharField(max_length=150)
+    service_description = models.TextField(max_length=500)
+    service_cost = models.PositiveIntegerField()
+    service_duration = models.PositiveIntegerField()
+    master = models.ForeignKey(Master, on_delete=models.CASCADE, related_name='master')
+    client = models.ManyToManyField(Clients)
+
+    def __str__(self):
+        return self.service_name
+
+
+class Sessions(models.Model):
+    datetime = models.DateTimeField()
+    service = models.ForeignKey(Services, on_delete=models.CASCADE)
+    client = models.ForeignKey(Clients, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.datetime
