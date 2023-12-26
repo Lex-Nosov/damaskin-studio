e857a6ca34874c27f1fda0d397833ffde4b8ec39
Lex <alexnosov2009@rambler.ru>
2023-12-26 14:01:08 +0300

    models done

--- main_app/models.py
+++ main_app/models.py
@@ -1,16 +1,6 @@
 from django.db import models
 
 
-class Clients(models.Model):
-    client_first_name = models.CharField(max_length=100)
-    client_second_name = models.CharField(max_length=100)
-    client_phone_number = models.CharField(max_length=20)
-    avatar = models.ImageField()
-
-    def __str__(self):
-        return f'{self.client_first_name}+' '+{self.client_second_name}'
-
-
 class Master(models.Model):
     master_first_name = models.CharField(max_length=50)
     master_second_name = models.CharField(max_length=50)
@@ -20,8 +10,20 @@ class Master(models.Model):
         return f'{self.master_first_name}{self.master_second_name}'
 
 
+class Clients(models.Model):
+    client_first_name = models.CharField(max_length=100)
+    client_second_name = models.CharField(max_length=100)
+    client_phone_number = models.CharField(max_length=20)
+    profile_image = models.ImageField(blank=True, null=True)
+    master = models.ForeignKey(Master, related_name='master', on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.client_first_name}+' '+{self.client_second_name}'
+
+
 class Sessions(models.Model):
     datetime = models.DateTimeField()
+    client = models.ForeignKey(Clients, related_name='session_client', on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.datetime
@@ -32,14 +34,7 @@ class Services(models.Model):
     service_description = models.TextField(max_length=500)
     service_cost = models.PositiveIntegerField()
     service_duration = models.PositiveIntegerField()
-    master = models.ManyToManyField(Master, related_name='master')
-    client = models.ManyToManyField(Clients, related_name='client')
-    session = models.ManyToManyField(Sessions, related_name='session')
+    client = models.ForeignKey(Clients, related_name='service_client', on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.service_name
-
-#
-# class Review(models.Model):
-#     person = models.ForeignKey(Clients, related_name='client', on_delete=models.CASCADE)
-#     description = models.TextField(max_length=1000)
